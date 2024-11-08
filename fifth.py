f1ad63667b00538d6cc8df18ad8a18efe63332a7
Sapate Vaibhav <sapatevaibhav@duck.com>
2024-11-08 19:20:14 +0530

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -1,35 +1,32 @@
 import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.applications import VGG16
-from tensorflow.keras.layers import Dense, Flatten, Dropout
-from tensorflow.keras.models import Model
+import cv2
 
-# Load CIFAR-10 dataset and preprocess
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-x_train, x_test = tf.image.resize(x_train / 255.0, (64, 64)), tf.image.resize(x_test / 255.0, (64, 64))
+# Load pre-trained model from TensorFlow Hub
+detector = hub.load("https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2")
 
-# Load pre-trained VGG16 model, freeze base layers
-base_model = VGG16(weights='imagenet', include_top=False, input_shape=(64, 64, 3))
-for layer in base_model.layers: layer.trainable = False
+# Load an internet image
+image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/017_Great_blue_turaco_at_Kibale_forest_National_Park_Photo_by_Giles_Laurent.jpg/500px-017_Great_blue_turaco_at_Kibale_forest_National_Park_Photo_by_Giles_Laurent.jpg"  # Example image URL
+image_path = tf.keras.utils.get_file("test_image2.jpg", origin=image_url)
 
-# Add custom classifier
-x = Flatten()(base_model.output)
-x = Dense(64, activation='relu')(x)
-x = Dropout(0.5)(x)
-x = Dense(10, activation='softmax')(x)
+# Load and preprocess the image
+img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+resized_img = cv2.resize(img, (320, 320))
+input_tensor = tf.convert_to_tensor([resized_img], dtype=tf.uint8)
 
-# Create and compile model
-model = Model(inputs=base_model.input, outputs=x)
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# Run detection
+detections = detector(input_tensor)
 
-# Train and evaluate model
-model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test), batch_size=64)
-test_acc = model.evaluate(x_test, y_test, verbose=0)[1]
-
-# Predict and visualize results
-predictions = model.predict(x_test)
-plt.imshow(x_test[10])
-plt.title(f"Pred: {predictions[10].argmax()}, Actual: {y_test[10][0]}")
+# Visualize detections
+plt.imshow(img)
+for i in range(detections['detection_scores'][0].numpy().size):
+    if detections['detection_scores'][0][i].numpy() >= 0.3:
+        bbox = detections['detection_boxes'][0][i].numpy()
+        y_min, x_min, y_max, x_max = bbox
+        h, w, _ = img.shape
+        plt.gca().add_patch(plt.Rectangle((x_min * w, y_min * h), (x_max - x_min) * w, (y_max - y_min) * h,
+                                          edgecolor='red', fill=False, linewidth=2))
+plt.axis("off")
 plt.show()
-
-print(f'Test accuracy: {test_acc * 100:.2f}%')
